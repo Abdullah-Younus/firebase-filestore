f502d288fd43929ab3b798590e31d9900ac3e36e
Abdullah-Younus <abdullah1001k@gmail.com>
2022-11-01 00:29:19 +0500

    login done

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ const db = firebase.firestore();
 
 var userData = [];
 
-function signup() {
+var form = document.getElementById('myForm');
+function handleForm(event) {
+    event.preventDefault();
     let obj = {
         name: document.getElementById('name').value,
         email: document.getElementById('email').value,
@@ -28,13 +30,33 @@ function signup() {
     };
     // event.preventDefault();
     db.collection("formdata").add(obj).then((docRef) => {
-        localStorage.setItem('userobj', JSON.stringify(obj));
         console.log("Document written with ID: ", docRef.id);
-        window.location.href = './login.html';
+        alert('SignUp success');
+        window.location.href = './index.html';
     }).catch((error) => {
         console.error("Error adding document: ", error);
     });
+
 }
+
+form.addEventListener('submit', handleForm);
+
+// function handleForm(e) {
+//     e.preventDefault();
+//     let obj = {
+//         name: document.getElementById('name').value,
+//         email: document.getElementById('email').value,
+//         password: document.getElementById('password').value,
+//     };
+//     // event.preventDefault();
+//     db.collection("formdata").add(obj).then((docRef) => {
+//         console.log("Document written with ID: ", docRef.id);
+//         alert('SignUp success');
+//         window.location.href = './index.html';
+//     }).catch((error) => {
+//         console.error("Error adding document: ", error);
+//     });
+// }
 // get data from Firebase
 function getData() {
     try {
@@ -52,77 +74,50 @@ function getData() {
 
 }
 
-// userData array current email password matching login function
-
-function login() {
-    // event.preventDefault();
-
+function handleLogin() {
     let obj = {
         email: document.getElementById('email').value,
         password: document.getElementById('password').value,
     };
-
-    let emailExist = userData.some(user => user.email === obj.email);
+    let emailExist = userData.some(user => user.email === obj.email); // ture ya false
     let passwordExist = userData.some(user => user.password === obj.password);
-
     if (emailExist && passwordExist) {
-        console.log('login success');
+        alert('login success');
+        window.location.href = './home.html';
     }
     else if (emailExist === false) {
-        console.log('email invalid');
+        alert('email invalid');
     }
     else if (passwordExist === false) {
-        console.log('password invalid');
+        alert('password invalid');
     } else {
-        console.log('invalid email or password');
+        alert('invalid email or password');
     }
-    // console.log('login failed');
-
-
-
-
-    // let index = userData.findIndex((user) => {
-    //     return user.email === obj.email && user.password === obj.password;
-    // });
-    // if (index > -1) {
-    //     localStorage.setItem('userobj', JSON.stringify(obj));
-    // }
-    // else {
-    //     console.log();
-    // }
-    // console.log('huamra obj', obj)
-    // for (i = 0; i < userData.length; i++) {
-    //     if ((userData[i].email === obj.email) && (userData[i].password === obj.password)) {
-    //         localStorage.setItem("curentuser", userData[i]);
-    //     }
-    //     else {
-    //         console.log("err");
-    //     }
-    // }
-
-    return false;
-
-    // userData.map((eachItem, index) => {
-    //     console.log(eachItem[index]);
-    // })
-    // for loop userData array
-    // for (let i = 0; i < userData.length; i++) {
-    //     console.log("Ya upper wala",Object. userData[i])
-    //     // if (userData[i].email === obj.email && userData[i].password === obj.password) {
-    //     //     console.log('user login success');
-    //     // }
-    //     // else if (userData[i].email !== obj.email) {
-    //     //     console.log('email invalid');
-    //     // }
-    //     // else if (userData[i].password !== obj.password) {
-    //     //     console.log('password invalid');
-    //     // }
-    //     // else {
-    //     //     console.log('email or password invalid');
-    //     // }
-    // }
-
 }
+
+
+// function login() {
+//     let obj = {
+//         email: document.getElementById('email').value,
+//         password: document.getElementById('password').value,
+//     };
+//     let emailExist = userData.some(user => user.email === obj.email);
+//     let passwordExist = userData.some(user => user.password === obj.password);
+
+//     if (emailExist && passwordExist) {
+//         alert('login success');
+//         window.location.href = './home.html';
+//     }
+//     else if (emailExist === false) {
+//         alert('email invalid');
+//     }
+//     else if (passwordExist === false) {
+//         alert('password invalid');
+//     } else {
+//         alert('invalid email or password');
+//     }
+//     return false;
+// }
 
 
 
