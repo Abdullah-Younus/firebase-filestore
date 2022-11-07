c3a8b3491d86bbfbced7a44019a59bcb575313e1
Abdullah-Younus <abdullah1001k@gmail.com>
2022-11-08 00:13:43 +0500

    changes

--- home.js
+++ home.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
     apiKey: "AIzaSyA002LsdsULgeS4oDzmJ8ckFg10_7UdNxQ",
     authDomain: "test-d3ff6.firebaseapp.com",
+    databaseURL: "https://test-d3ff6-default-rtdb.firebaseio.com/",
     projectId: "test-d3ff6",
-    storageBucket: "test-d3ff6.appspot.com",
     messagingSenderId: "620970481245",
     appId: "1:620970481245:web:daaba434f907f09e38dfb4",
     measurementId: "G-ERP7EDK9VF",
@@ -12,14 +12,16 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
+
 var allformData = [];
 var result = [];
 async function getallData() {
     try {
         await db.collection("createform").get().then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
-                // console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
-                allformData.push(doc.data());
+                let user = doc.data();
+                user.id = doc.id;
+                allformData.push(user);
             });
             localStorage.setItem("all", JSON.stringify(allformData));
             // console.log('All Data Get', allformData);
@@ -45,23 +47,45 @@ function check() {
     alluserdata.forEach((user) => {
         document.getElementById('card').innerHTML += `
         <div style="display: flex;align-items: center;flex-direction: column;width:25%;height:100%;padding:5px;margin:5px"> 
-            <img src="${user.img}" style="width:300px;height:200px;" />
-            <br/>
-            <br/>   
-            <label>Title field:${user.txt_field}</label>
-            <label>Title Item:${user.title_item}</label>
-            <label>Product Search:${user.product_search}</label>
-            <label>Description:${user.des_item}</label>
-            <label>Description:${user.date}</label>
-            <label><b>Posted By:${user.user}</b></label>
+            
+                <img src="${user.img}" style="width:300px;height:200px;" />
+                <br/>
+                <br/>   
+                <label>Title field:${user.txt_field}</label>
+                <label>Title Item:${user.title_item}</label>
+                <label>Product Search:${user.product_search}</label>
+                <label>Description:${user.des_item}</label>
+                <label>Description:${user.date}</label>
+                <label><b>Posted By:${user.user}</b></label>
+                <button type="button" onclick="return handleMessage('./message.html'+'?'+'${user.id}','${user.id}')">Message</button>
         </div>
         `
     })
-
-
 }
 
+// function handleMessage(url, id) {
+//     console.log('ID CHECK', id);
+//     console.log('URL ', url);
+//     window.location.href = "./message.html"
+//     // window.open(url, '_blank');
+// }
 
+// function sendMessage() {
+//     var url = document.URL;
+//     var url_array = url.split('?') // Split the string into an array with / as separator
+//     var url_id = url_array[url_array.length - 1];  // Get the last part of the array (-1)
+//     let myName = localStorage.getItem('currentUser');
+//     console.log(url_id);
+//     // var id = url.substring(url.lastIndexOf('?') + 1);
+//     let message = document.getElementById('message').value;
+//     console.log('Message :', message);
+//     // firebase.database().ref("messages").push().set({
+//     //     "sender": myName,
+//     //     "message": message
+//     // });
+//     // prevent form from submitting
+//     // return false;
+// }   
 function response() {
     const found = document.getElementById('found').checked;
     // const lost = document.getElementById('lost').checked;
