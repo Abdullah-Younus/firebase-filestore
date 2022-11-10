0f093f6ef2d2acead2468a957f38420d8b8e0f46
Abdullah-Younus <abdullah1001k@gmail.com>
2022-11-10 23:04:11 +0500

    local changes

--- home.js
+++ home.js
@@ -19,17 +19,11 @@ async function getallData() {
     try {
         await db.collection("createform").get().then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
-                if (allformData !== undefined) {
-                    let user = doc.data();
-                    user.id = doc.id;
-                    allformData.push(user);
-                    check(allformData)
-                    localStorage.setItem("all", JSON.stringify(allformData));
-                } else {
-                    return allformData = "NOT ANY DATA";
-                }
+                let user = doc.data();
+                user.id = doc.id;
+                allformData.push(user);
             });
-
+            localStorage.setItem("all", JSON.stringify(allformData));
             // console.log('All Data Get', allformData);
         });
 
@@ -40,83 +34,66 @@ async function getallData() {
 }
 
 
-
-
 // allformData get all values function print html tag?
 
-function check(user) {
+function check() {
     // console.log('All Data Get', allformData);
-    // let users = allformData;
-    // let alluserdata = JSON.parse(allformData);
-    if (user) {
-        user.forEach((user) => {
-            console.log('Ya Wala OBJ SEBDER', user);
-            document.getElementById('card').innerHTML += `
-            <div style="display: flex;align-items: center;flex-direction: column;width:25%;height:100%;padding:5px;margin:5px"> 
-    
-                    <img src="${user.img}" style="width:300px;height:200px;" />
-                    <br/>
-                    <br/>   
-                    <label>Title field:${user.txt_field}</label>
-                    <label>Title Item:${user.title_item}</label>
-                    <label>Product Search:${user.product_search}</label>
-                    <label>Description:${user.des_item}</label>
-                    <label>Description:${user.date}</label>
-                    <label><b>Posted By:${user.user}</b></label>
-                    <button type="button" onclick="return handleMessage('./message.html'+'?'+'${user.userId}','${user.userId}')">Chat</button>
-            </div>
-            `
-        })
-    }
+    let users = localStorage.getItem("all");
+    let alluserdata = JSON.parse(users);
+    console.log('All Data', alluserdata);
 
-
+    alluserdata.forEach((user) => {
+        console.log('Ya Wala OBJ SEBDER', user);
+        document.getElementById('card').innerHTML += `
+        <div style="display: flex;align-items: center;flex-direction: column;width:25%;height:100%;padding:5px;margin:5px"> 
+            
+                <img src="${user.img}" style="width:300px;height:200px;" />
+                <br/>
+                <br/>   
+                <label>Title field:${user.txt_field}</label>
+                <label>Title Item:${user.title_item}</label>
+                <label>Product Search:${user.product_search}</label>
+                <label>Description:${user.des_item}</label>
+                <label>Description:${user.date}</label>
+                <label><b>Posted By:${user.user}</b></label>
+                <label><b>USER ID:${user.userId}</b></label>
+                <button type="button" onclick="return handleMessage('./message.html'+'?'+'${user.userId}','${user.userId}')">Message</button>
+        </div>
+        `
+    })
 }
 
-// function handleDelete(id) {
-//     let users = localStorage.getItem("all");
-//     let alluserdata = JSON.parse(users);
-//     console.log('All Data', alluserdata);
-//     // localstorage array of multiply objects one index delete?
-
-//     //remove object
-//     for (var i = 0; alluserdata.length; i++) {
-//         var data = alluserdata.data[i];
-//         if (i === id) {
-//             alluserdata.data.splice(data, 1);
-//         }
-//     }
-
-//     console.log();
-
-//     // alluserdata.forEach((user) => {
-//     //     if (user.id === id) {
-//     //         document.getElementById('card').innerHTML = "";
-//     //     }
-//     // });
-//     // db.collection("createform").doc(id).delete().then(() => {
-//     //     console.log("Document successfully deleted!");
-//     // }).catch((error) => {
-//     //     console.error("Error removing document: ", error);
-//     // });
-
-// }
-
 function handleMessage(url, id) {
+    // update read status
+    firebase.database().ref('messages').on('value', (snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+            if (childSnapshot.val().id === id) {
+                firebase.database().ref('messages/' + childSnapshot.key).update({
+                    read: true
+                })
+            }
+        })
+    })
+
+
+
     console.log('ID CHECK', id);
     console.log('URL ', url);
     // window.location.href = "./message.html"
     window.open(url, '_blank');
+
 }
 
 function sendMessage() {
     var url = document.URL;
     var url_array = url.split('?') // Split the string into an array with / as separator
     var receiverId = url_array[url_array.length - 1];  // Get the last part of the array (-1)
+
     // find user from local storage by receiverId
     let users = localStorage.getItem("userData");
     let alluserdata = JSON.parse(users);
     let user = alluserdata.find((user) => user.id === receiverId);
-    // return console.log('user :', user);
+
     let myName = localStorage.getItem('currentUser');
     let currentUser = JSON.parse(myName);
     console.log(currentUser.name);
@@ -126,8 +103,10 @@ function sendMessage() {
     firebase.database().ref("messages").push().set({
         receiverId: receiverId,
         receiverName: user.name,
+
         senderId: currentUser.id,
         senderName: currentUser.name,
+
         message: message,
         read: false,
         timestamp: Date.now(),
@@ -140,13 +119,16 @@ function allMessage() {
     var url_array = url.split('?') // Split the string into an array with / as separator
     var receiverId = url_array[url_array.length - 1];
     console.log('receiverId', receiverId);
+
     let myName = localStorage.getItem('currentUser');
     let currentUser = JSON.parse(myName);
     let senderId = currentUser.id;
     const dbRef = firebase.database().ref();
+
     dbRef.child("messages").on("value", function (snapshot) {
         // put all messages in array
         let messages = [];
+
         // read messages and update read status
         snapshot.forEach(function (childSnapshot) {
             let childData = childSnapshot.val();
@@ -155,14 +137,17 @@ function allMessage() {
                 firebase.database().ref('messages/' + childSnapshot.key).update({ read: true });
             }
         });
+
         // filter messages by sender and receiver
         let filteredMessages = messages.filter((message) => {
             return (message.senderId == senderId && message.receiverId == receiverId) || (message.senderId == receiverId && message.receiverId == senderId);
         });
+
         // sort messages by timestamp
         filteredMessages.sort((a, b) => {
             return b.timestamp - a.timestamp;
         });
+
         // show messages in html with delete button
         document.getElementById('messages').innerHTML = filteredMessages.map((message) => {
             return `<div class="message">
@@ -230,7 +215,6 @@ function response() {
 
     console.log("found", found);
     console.log("lost", lost);
-    console.log("Result", result);
 }
 
 function lost() {
@@ -279,3 +263,85 @@ function lost() {
     }
 }
 
+// let result = [];
+function getNewMessages() {
+
+    let user = localStorage.getItem("currentUser");
+    let currentUser = JSON.parse(user);
+
+    firebase.database().ref('messages').on('value', (snapshot) => {
+        const messages = snapshot.val();
+        const filteredMessages = Object.keys(messages).map((key) => {
+            return messages[key];
+        }).filter((message) => {
+            return (message.receiverId == currentUser.id && message.read === false);
+        });
+
+        console.log("filteredMessages: ", filteredMessages);
+
+        // filter messages groupby senderId and count them
+        let groupedMessages = filteredMessages.reduce((r, a) => {
+            r[a.senderId] = [...r[a.senderId] || [], a];
+            return r;
+        }, {});
+
+        console.log("groupedMessages: ", groupedMessages);
+
+
+        // group messages by senderId and count them
+        let groupedMessagesCount = Object.keys(groupedMessages).map((key) => {
+            return {
+                senderId: key,
+                senderName: groupedMessages[key][0].senderName,
+                count: groupedMessages[key].length
+            }
+        });
+
+        console.log("groupedMessagesCount: ", groupedMessagesCount);
+        // showUsers(groupedMessagesCount);
+
+        // console.log('notificationData: ', notificationData);
+        document.getElementById('usersList').innerHTML = "";
+
+        // console.log("finalUsers: ", finalUsers);
+        groupedMessagesCount.forEach((user) => {
+            document.getElementById('usersList').innerHTML += `
+                        <div class="user" onclick="handleMessage('./message.html'+'?'+'${user.senderId}','${user.senderId}')" style="display: flex; justify-content: space-between; align-items: center;">
+                            <h5>${user.senderName}</h5>
+                            <p>${user.count}</p>
+                        </div>`
+        });
+    })
+}
+
+// function childChange() {
+//     // get user from local storage
+//     let user = localStorage.getItem("currentUser");
+//     let currentUser = JSON.parse(user);
+
+//     // when message is read, update the message status to read
+//     firebase.database().ref('messages').on('child_changed', (snapshot) => {
+//         const message = snapshot.val();
+//         if (message.receiverId === currentUser.id && message.read === true) {
+//             firebase.database().ref('messages/' + message.id).update({
+//                 read: true
+//             });
+//         }
+//     });
+
+//     // update the notification count
+//     getNewMessages();
+
+// }
+
+function showUsers() {
+    let users = localStorage.getItem("userData");
+    let allUsers = JSON.parse(users);
+    // append users to usersList innerHTML
+    allUsers.forEach((user) => {
+        document.getElementById('allUsers').innerHTML += `
+            <div class="user" onclick="handleMessage('./message.html'+'?'+'${user.id}','${user.id}')">
+                <p>${user.name}</p>
+            </div>`
+    });
+}
